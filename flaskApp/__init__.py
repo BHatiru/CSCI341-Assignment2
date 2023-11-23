d69d3883f3f99b37edb93abdcbedde97160d84c0
Batyr Bodaubay <batyr.bodaubay@nu.edu.kz>
2023-11-24 00:36:13 +0600

    Update __init__.py

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+
 
 app = Flask(__name__)
 #create secret key
@@ -11,5 +11,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
 from flaskApp import views
