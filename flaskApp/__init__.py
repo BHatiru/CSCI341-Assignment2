18b36ace6afceff2f873fb5256d208a43fe9f15d
Batyr Bodaubay <batyr.bodaubay@nu.edu.kz>
2023-11-24 06:19:46 +0600

    migration to sql cloud db

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -5,10 +5,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
+user = 'postgres'
+password = '030507500301'
+database = 'data'
+cloud_sql_connection_name = 'projectcsci341-406020:europe-central2:postgres-db-csci341'
+
+
 #create secret key
 app.config['SECRET_KEY'] = 'babatyr123'
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.db')
+
+tmp_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tmp'))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/data.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{user}:{password}@/cloudsql/{cloud_sql_connection_name}/{database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
